5e076aecb6d6dd40faaf2e463fe2ad84535c063c
Trần Khánh Toàn <trankhanhtoan321@gmail.com>
2017-03-31 08:29:22 +0700

    update

--- app/logs/log-2017-03-19.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-03-19 14:08:00 --> 404 Page Not Found: Faviconico/index
--- app/logs/log-2017-03-23.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-03-23 09:09:35 --> 404 Page Not Found: Faviconico/index
